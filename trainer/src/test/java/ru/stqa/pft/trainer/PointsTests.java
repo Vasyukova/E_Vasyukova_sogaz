d6d2931f361ea5a98c7c3d96a846c286d940d9b0
Vasyukova <63801351+Vasyukova@users.noreply.github.com>
2020-05-01 20:07:09 +0300

    Test_Square_Point

--- trainer/src/test/java/ru/stqa/pft/trainer/PointsTests.java
+++ trainer/src/test/java/ru/stqa/pft/trainer/PointsTests.java
@@ -1,6 +1,15 @@
 package ru.stqa.pft.trainer;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
 public class PointsTests
 {
-
+    public void testPoints()
+    {
+        Point p1 = new Point(10, 60);
+        Point p2 = new Point(50, 30);
+        Assert.assertEquals((p1.distance(p2)), 40.0);
+    }
 }
--- trainer/src/test/java/ru/stqa/pft/trainer/SquareTests.java
+++ trainer/src/test/java/ru/stqa/pft/trainer/SquareTests.java
@@ -1,4 +1,14 @@
 package ru.stqa.pft.trainer;
 
-public class SquareTests {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class SquareTests
+{
+    public void testArea()
+    {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25.0);
+    }
 }
