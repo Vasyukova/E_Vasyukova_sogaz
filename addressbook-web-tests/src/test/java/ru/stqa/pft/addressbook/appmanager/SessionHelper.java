324eb5053f4eb094b3d9656292abc85857bfca3d
Vasyukova <63801351+Vasyukova@users.noreply.github.com>
2020-05-06 16:52:32 +0300

    Update SessionHelper.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -15,10 +15,11 @@ public class SessionHelper
     }
 
     public void login(String login, String password) {
+        wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(login);
+        wd.findElement(By.name("user")).sendKeys("admin");
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.id("LoginForm")).submit();
     }
 }
